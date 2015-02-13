6f6bb34530306b5cfcc3bfd2f14eb06b753ac06b
Adam Kemp <github@adamkemp.com>
2015-02-13 16:42:22 -0600

    Add Android UI.

---
+++ RadixConverter.Android/RadixConverterActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Cirrious.MvvmCross.Droid.Views;
+using RadixConverter.ViewModel;
+
+namespace RadixConverter.Android
+{
+    [Activity(Label = "Radix Converter", MainLauncher = true)]
+    public class RadixConverterActivity : MvxActivity
+    {
+        public new RadixConverterViewModel ViewModel
+        {
+            get { return (RadixConverterViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
+        protected override void OnViewModelSet()
+        {
+            base.OnViewModelSet();
+            SetContentView(Resource.Layout.RadixConverter);
+        }
+    }
+}
+
---
+++ RadixConverter.Android/Setup.cs
@@ -0,0 +1,20 @@
+﻿using Android.Content;
+using Cirrious.MvvmCross.Droid.Platform;
+using Cirrious.MvvmCross.ViewModels;
+using RadixConverter.ViewModel;
+
+namespace RadixConverter.Android
+{
+    public class Setup : MvxAndroidSetup
+    {
+        public Setup(Context applicationContext)
+            : base(applicationContext)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+    }
+}
